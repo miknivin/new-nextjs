a566f9f1ca51db42191d873b7e056c9a2ce3daee
miknivin <116654570+miknivin@users.noreply.github.com>
2024-11-22 15:44:16 +0530

    new nextjs proj

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  console.log("Hello");
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -80,6 +83,7 @@ export default function Home() {
           />
           Examples
         </a>
+        <Link  href="/about">about</Link>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
